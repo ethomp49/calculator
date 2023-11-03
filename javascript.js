751db72ac51106badfec7116817ca95d98de088d
Eric Thompson <thompsoneric3259@gmail.com>
2023-11-03 09:08:30 -0400

    Improve calculator function
    I forgot to commit for a while. Changes include stringing operations together and modifications to the logic of the input display.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-document.querySelector(".num-buttons").addEventListener("click", (e) => buttonEventHandler(e));
+document.querySelector(".buttons").addEventListener("click", (e) => buttonEventHandler(e));
 
 function add(a, b) {
     return a + b;
@@ -18,7 +18,7 @@ function multiply(a, b) {
 
 function calculate(a, b, operator) {
     const operation = getOperation(operator);
-    return operation(a, b);
+    return Math.round(operation(a, b) * 1000) / 1000;
 }
 
 function getOperation(operator) {
@@ -41,11 +41,10 @@ function buttonEventHandler(e) {
     const button = e.target;
     const buttonFunction = getButtonFunction(button.id);
     if (!(buttonFunction === undefined)) {
-        buttonFunction(button.id);
+        const input = getInputDisplay();
+        buttonFunction(input, button.id);
     }
 }
-
-
 
 function getButtonFunction(id) {
     switch (id) {
@@ -75,35 +74,105 @@ function getButtonFunction(id) {
     }
 }
 
-function giveNumber(number) {
-    const currentInput = getDisplayInput();
-    const newInput = (currentInput !== '0') ? 
-        currentInput + number :
-        number;
-    setDisplayInput(newInput);        
+function giveNumber(currentInput, number) {
+    if (hasResult()) {
+        setResultDisplay('');
+    }
+
+    setInputDisplay(currentInput + number);        
 }
 
-function giveOperator(operator) {
-    const currentInput = getDisplayInput();
-    const newInput = (currentInput.search(/[-+/*]/) === -1) ?
-        currentInput + ` ${operator} ` :
-        currentInput.replace(/[-+/*]/, operator);
+function giveOperator(currentInput, operator) {
+    let newInput;
 
-    setDisplayInput(newInput);
+    if (hasResult()) {
+        newInput = getResult() + ` ${operator} `;
+        setResultDisplay('');
+    } else if (hasOperator(currentInput)) {  //case: "38 * "
+        if (lastNumber(currentInput) === '') {
+            newInput = currentInput.replace(
+                getOperator(currentInput), operator);
+        } else {
+            const a = parseFloat(getA(currentInput))
+                , b = parseFloat(getB(currentInput))
+                , op = getOperator(currentInput);   
+            newInput = calculate(a, b, op) + ` ${operator} `;
+        }
+    } else {
+        newInput = currentInput + ` ${operator} `;
+    }
+    setInputDisplay(newInput);
 }
 
-function giveDecimal() {
-
+function giveDecimal(currentInput) {
+    if (hasResult()) {
+        setResultDisplay('');
+        setInputDisplay('0.');
+    } else if (noDecimal(currentInput)) {
+        setInputDisplay((lastNumber(currentInput) === '') ?
+            currentInput + "0." :
+            currentInput + ".");
+    }
 }
 
-function giveEquals() {
-
+function giveEquals(input) {
+    const a = parseFloat(getA(input));
+    let result;
+    if (hasOperator(input)) {
+        const b = parseFloat(getB(input));
+        const op = getOperator(input);
+        result = calculate(a, b, op);
+    } else {
+        result = a;
+    }
+    setResultDisplay(`= ${result}`);
 }
 
-function getDisplayInput() {
+function getInputDisplay() {
     return document.querySelector(".input-display").textContent;
 }
 
-function setDisplayInput(input) {
+function setInputDisplay(input) {
     document.querySelector(".input-display").textContent = input;
+}
+
+function hasOperator(input) {
+    return input.search(/[-+/*]/) >= 0;
+}
+
+function getOperator(input) {
+    return input.split(" ")[1];
+}
+
+function getA(input) {
+    return input.split(" ")[0];
+}
+
+function getB(input) {
+    return input.split(" ")[2];
+}
+
+function setResultDisplay(string) {
+    document.querySelector(".result-display").textContent = string;
+}
+
+function hasResult() {
+    return !(getResultDisplay() === '');
+}
+
+function getResultDisplay() {
+    return document.querySelector(".result-display").textContent;
+}
+
+function getResult() {
+    return getResultDisplay().split(" ")[1];
+}
+
+function noDecimal(input) {
+    return !(lastNumber(input).includes("."));
+}
+
+function lastNumber(input) {
+    const pieces = input.split(" ");
+    return pieces[pieces.length - 1];
 }
